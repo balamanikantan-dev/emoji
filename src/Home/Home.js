08efaba0bde282efd89ffe21a062a27f0f56a156
balamanikantan.dev <balamanikantan.dev@gmail.com>
2021-03-03 15:24:30 +0530

    optimized

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,21 +1,28 @@
 import { Card, Col, notification, Row } from 'antd'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import './Home.css'
 import Data from "../Json/Data.json"
 
 const Home = () => {
-    const [emojis, setEmojis] = useState(Data)
-    const changeHandler = (event) => {
-        console.log(event.target.value)
-        if (event.target.value === '') {
-            setEmojis(Data)
+    // const [emojis, setEmojis] = useState(Data)
+    const [query, setQuery] = useState('')
+
+    const emojis = useMemo(() => {
+        if (query === '') {
+            return (Data)
 
         } else {
+            console.time('a')
             const results = Data.filter(emoji => {
-                return emoji.keywords.search(event.target.value) !== -1 || emoji.title.search(event.target.value) !== -1
+                return emoji.keywords.search(query) !== -1 || emoji.title.search(query) !== -1
             })
-            setEmojis(results)
+            console.timeEnd('a')
+            return (results)
         }
+    }, [query])
+    const changeHandler = (event) => {
+        console.log(event.target.value)
+        setQuery(event.target.value)
     }
     const clickHandler = (text) => {
         navigator.clipboard.writeText(text).then(function () {
@@ -42,7 +49,6 @@ const Home = () => {
                         return <Col key={emoji.title} xs={24} sm={12} md={8} lg={6} xl={6}>
                             <Card onClick={() => clickHandler(emoji.symbol)} title={emoji.title} bordered={false} style={{ width: 300, fontSize: "50px", textAlign: 'center', marginLeft: "80px" }}>
                                 <p>{emoji.symbol}</p><br></br>
-
                             </Card>
                         </Col>
                     })}
